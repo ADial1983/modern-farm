6b1364751f4f4cefb66dabb9c34cc602a435738c
Al Dial <dial.allison@comcast.net>
2022-10-07 10:54:22 -0500

    first commit

---
+++ src/scripts/seeds/sunflower.js
@@ -0,0 +1,8 @@
+export const createSunflower= () => {
+    let sunflower = {
+        type: 'sunflower',
+        height: 380,
+        output: 3
+    }
+    return sunflower
+}
---
+++ src/scripts/seeds/wheat.js
@@ -0,0 +1,8 @@
+export const createWheat = () => {
+    let wheat = {
+        type: 'wheat',
+        height: 230,
+        output: 6
+    }
+    return asparagus
+}
